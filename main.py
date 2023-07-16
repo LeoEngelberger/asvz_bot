97d0aacee26b542b29961f91a0f61386d9e811cd
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-07-16 17:46:40 +0200

    getting all lesson information

--- main.py
+++ main.py
@@ -13,6 +13,15 @@
 #  limitations under the License.
 
 import http.client
+import json
+
+# env vars #
+sorted_entries = {}
+payload_for_fahrplan = ''
+headers_for_fahrplan = {}
+lessons_with_name = {}
+lessons_of_type = []
+
 
 # get id from detail view on schalter.asvz.ch exp: https://schalter.asvz.ch/tn/lessons/534848
 course_id = "534848"
@@ -21,17 +30,63 @@ course_id = "534848"
 # it's the really long encrypted string
 authorization_token = \
  "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-conn = http.client.HTTPSConnection("schalter.asvz.ch")
-payload = ''
-headers = {
-  'Authorization': f'Bearer {authorization_token}'
-}
 
-url = "/tn-api/api/Lessons/%s/Enrollment" % course_id
 
-print(url)
+def get_all_lessons():
+    conn_to_sportfahrplan = http.client.HTTPSConnection("asvz.ch")
+    conn_to_sportfahrplan.request("GET", "/asvz_api/event_search?_format=json&limit=2000", payload_for_fahrplan,
+                                  headers_for_fahrplan)
+    res_sportfahrplan = conn_to_sportfahrplan.getresponse()
+    sportfahrplan_data = res_sportfahrplan.read()
+    parsed_data = json.loads(sportfahrplan_data)
+    entries = parsed_data['results']
+    # Create a dictionary to store the filtered and sorted entries
 
-conn.request("POST", url, payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+    # Loop through the entries
+    for entry in entries:
+        sport_name = entry['sport_name']
+        # Check if the sport_name already exists as a key in the dictionary
+        if sport_name in sorted_entries:
+            # If it does, append the entry to the existing list of entries
+            sorted_entries[sport_name].append(entry)
+        else:
+            # If it doesn't, create a new list with the entry as the first element
+            sorted_entries[sport_name] = [entry]
+
+
+def find_all_lessons_with_name(name):
+    global lessons_with_name
+    all_lessons_with_name = None
+    for entry in sorted_entries:
+        if entry == name:
+            all_lessons_with_name = sorted_entries[entry]
+    lessons_with_name = all_lessons_with_name
+
+
+def find_lessons_of_type():
+    global lessons_of_type
+    for lesson in lessons_with_name:
+        print(lesson['title'])
+        if lesson['title'] == "Akrobatik":
+            lessons_of_type.append(lesson)
+
+
+def enroll_in_lesson():
+    conn = http.client.HTTPSConnection("schalter.asvz.ch")
+    payload = '_format=json&limit=60'
+    headers = {
+        'Authorization': f'Bearer {authorization_token}'
+    }
+
+    url = "/tn-api/api/Lessons/%s/Enrollment" % course_id
+    print(url)
+    conn.request("POST", url, payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+
+get_all_lessons()
+find_all_lessons_with_name("Manege")
+find_lessons_of_type()
+print(sorted_entries)

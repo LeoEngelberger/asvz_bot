805c8d69019ff96a2f764f3f1b3f18683ffa7b38
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-07-16 23:57:22 +0200

    Release Of Semi Automated Lesson Booking Bot
    # Description
    
    enrols you into all lessons meeting your predefined criteria
    
    run the code every time you want to book lessons
    
    by default, it will ask for confirmation before booking your lessons
    
    # Setup
    
    ## 1. Preparations
    
    ### 1.1 install Python
    Honestly if you don't know how to figure this one out this bot is not for you.
    make sure to add your python installation to your PATH variable
    
    ### 1.2 download this repo
    on the main repo page there is a button code click on it and download it all as a zip file.
    
    ## 1.3 unpack
    Unpack the entire zip archive into a folder of your choice
    
    ## 2. configuration
    
    ### 2.1 update your config.json
    - Open the config.json file in a texteditor of your choice. just make sure to save the file as a json file when you're done editing it.
    ###### What does it mean?
    
    `"relevant_days": ["Monday", "Wednesday"],` || Enter The days you want to go to your lessons
    `"course_name": "Manege",`  || Enter the name of your sportart
    `"type_name": "Akrobatik",`  || Enter the specific type of lesson you are looking for
    `"type_matter": true,` || if there are no types of lessons, or you just want to go to EVERYTHING with the name above, write false
    `"location_matter": true,`  || if you don't care about the location write false
    `"locations": "CAB Move", "ASVZ Sport Center",`  || list all the locations you want to go to
    `"token": "------------------------------",`  || follow the instruction in 1.2
    
    ### 2.2 get your token
    - go to schalter.asvz.ch, login and go to your membership overview. once there, right-click anywhere on the page and select inspect. in the window that opened on the right, look for the Network tab.
    - .Once there look for a package called MemberPerson that is not shown in red, click on it to select it and look for the wall of random letters. this is your token.
    - triple-click on it to select it in its entirety and copy it.
    - paste your token in instead of the dashes make sure you include the "Bearer " part with all Spaces.
    
    # Run
    ## 1 start the bot
    
    ### 1.1 open the folder you unpacked the bot into. right click and open the terminal.
    
    ### 1.2 start the bot
    enter the following command into your terminal:
    'python asvz_bot.py'
    
    ## 2 confirm enrolments
    
    ### 2.1 All or Individual
    The Bot first lists all possible classes and asks you if you want to book all or choose specific classes, and you are done, if you type n or no read 2.2
    **if you type in y or yes it WILL enrol you in every class and the ASVZ is counting on you.
    ###### DISCLAIMER: I do not take any responsibility for classes you missed. Check your enrolments regularly to ensure you do not overbook**
    
    ### 2.2 select individual lessons
    The bot will list every lesson with its time, location, and name. if you enter y or yes you are enrolled, if you enter n or no you're not.
    **DISCLAIMER: I do not take any responsibility for classes you missed. Check your enrolments regularly to ensure you do not overbook

--- main.py
+++ main.py
@@ -14,35 +14,83 @@
 
 import http.client
 import json
-from datetime import datetime
-
-# env vars #
-sorted_entries = {}
-payload_for_fahrplan = ''
-headers_for_fahrplan = {}
-lessons_with_name = {}
-lessons_of_type = []
-relevant_lessons = []
-relevant_course_ids = []
+from datetime import datetime, timezone, timedelta
 
 # config
-relevant_days = ["Monday", "Wednesday"]
-type_matter = True
-type_name = "Akrobatik"
-course_name = "Manege"
+with open('config.json') as config_file:
+    config_data = json.load(config_file)
 
-# get id from detail view on schalter.asvz.ch exp: https://schalter.asvz.ch/tn/lessons/534848
-course_id = "534848"
+relevant_days = config_data["relevant_days"]
+type_matter = config_data["type_matter"]
+type_name = config_data["type_name"]
+course_name = config_data["course_name"]
+location_matter = config_data["location_matter"]
+locations = config_data["locations"]
+authorization_token = config_data["token"]
 
-# get auth token by looking for member_info package under network in the inspector of your browser of choice
-# it's the really long encrypted string
-authorization_token = \
- "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+def convert_time(from_date):
+    date_str = from_date
+    comprehensive_from_date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+    your_timezone_offset = timedelta(hours=2)
+    your_timezone = timezone(your_timezone_offset)
+
+    # Calculate the time difference between UTC and your local time zone
+    time_difference = comprehensive_from_date.replace(tzinfo=timezone.utc) - comprehensive_from_date.replace(
+        tzinfo=your_timezone)
+
+    # Convert the datetime object to your local time zone
+    localized_from_date = comprehensive_from_date + time_difference
+    return localized_from_date.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+
+def get_course_id(lesson):
+    lesson_url = lesson['url']
+    split_url = lesson_url.split('/')
+    return split_url[-1]
+
+
+def print_lesson_information(lesson):
+    print("Name: " + lesson['sport_name'] + "|| Date: " + convert_time(lesson['from_date']) + "|| Location: " + lesson[
+        'location'])
+
+
+def print_lessons_enrolling_to(relevant_lessons):
+    for lesson in relevant_lessons:
+        print("Name: " + lesson['sport_name'] + "|| Date & Start Time: " + convert_time(
+            lesson['from_date']) + "|| Location: " + lesson['location'])
+
+
+def confirm_enrollment_in_all():
+    while True:
+        user_input = input("Do you want to enroll in all classes? (y/yes or n/no): ").strip().lower()
+        if user_input == 'y' or user_input == 'yes':
+            return True
+        elif user_input == 'n' or user_input == 'no':
+            return False
+        else:
+            print(
+                "Invalid input. Please enter 'y' or 'yes' to enroll in all classes, or 'n' or 'no' to enroll in individual classes.")
+
+
+def confirm_enrollment_in_single_lesson(relevant_lesson):
+    while True:
+        user_input = input("Do you want to enroll in this class (y/yes or n/no): ").strip().lower()
+        if user_input == 'y' or user_input == 'yes':
+            return True
+        elif user_input == 'n' or user_input == 'no':
+            return False
+        else:
+            print(
+                "Invalid input. Please enter 'y' or 'yes' to enroll in all classes, or 'n' or 'no' to enroll in individual classes.")
 
 
 def get_all_lessons():
+    payload_for_fahrplan = ''
+    headers_for_fahrplan = {}
+    sorted_entries = {}
     conn_to_sportfahrplan = http.client.HTTPSConnection("asvz.ch")
-    conn_to_sportfahrplan.request("GET", "/asvz_api/event_search?_format=json&limit=500", payload_for_fahrplan,
+    conn_to_sportfahrplan.request("GET", "/asvz_api/event_search?_format=json&limit=50", payload_for_fahrplan,
                                   headers_for_fahrplan)
     res_sportfahrplan = conn_to_sportfahrplan.getresponse()
     sportfahrplan_data = res_sportfahrplan.read()
@@ -60,66 +108,100 @@ def get_all_lessons():
         else:
             # If it doesn't, create a new list with the entry as the first element
             sorted_entries[sport_name] = [entry]
+    return sorted_entries
 
 
-def find_all_lessons_with_name():
-    global lessons_with_name
-    all_lessons_with_name = None
-    for entry in sorted_entries:
-        if entry == course_name:
-            all_lessons_with_name = sorted_entries[entry]
-    lessons_with_name = all_lessons_with_name
-
-
-def find_lessons_of_type():
-    global lessons_of_type
-    for lesson in lessons_with_name:
-        print(lesson['title'])
-        if lesson['title'] == type_name:
-            lessons_of_type.append(lesson)
-
-
-def enroll_in_lesson():
+def enrol_in_lesson(course_id):
     conn = http.client.HTTPSConnection("schalter.asvz.ch")
     payload = '_format=json&limit=60'
-    headers = {
-        'Authorization': f'Bearer {authorization_token}'
-    }
+    headers = {'Authorization': authorization_token}
 
     url = "/tn-api/api/Lessons/%s/Enrollment" % course_id
-    print(url)
     conn.request("POST", url, payload, headers)
     res = conn.getresponse()
     data = res.read()
-    print(data.decode("utf-8"))
+    data.decode("utf-8")
 
 
-def find_relevant_courses():
+def find_all_lessons_with_name(sorted_entries):
+    for entry in sorted_entries:
+        if entry == course_name:
+            return sorted_entries[entry]
+
+
+def filter_lessons_by_type(lessons_with_name):
+    lessons_of_type = []
+    for lesson in lessons_with_name:
+        if lesson['title'] == type_name:
+            lessons_of_type.append(lesson)
+    return lessons_of_type
+
+
+def filter_by_location(relevant_lessons):
+    filtered_lessons = []
+    for lesson in relevant_lessons:
+        if lesson['location'] in locations:
+            filtered_lessons.append(lesson)
+    return filtered_lessons
+
+
+def filter_by_date(lessons_of_type):
+    relevant_lessons = []
+    current_datetime = datetime.now()
     # Map the integer to the corresponding weekday name
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     for lesson in lessons_of_type:
         date_str = lesson['from_date']
-        comprehensive_date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        comprehensive_from_date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        date_str = lesson['oe_from_date']
+        oe_from_date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
         # Get the weekday as an integer (Monday is 0, Sunday is 6)
-        weekday_number = comprehensive_date.weekday()
+        weekday_number = comprehensive_from_date.weekday()
         weekday_name = weekdays[weekday_number]
-        if weekday_name in relevant_days:
+        if weekday_name in relevant_days and oe_from_date < current_datetime:
             relevant_lessons.append(lesson)
-    return relevant_days
-
-def get_course_ids():
-    global course_id
-    for lesson in relevant_lessons:
-        lesson_url = lesson['url']
-        split_url = lesson_url.split('/')
-        relevant_course_ids.append(split_url[-1])
-    return relevant_course_ids
+    return relevant_lessons
 
 
-get_all_lessons()
-find_all_lessons_with_name()
-if type_matter:
-    find_lessons_of_type()
-find_relevant_courses()
-get_course_ids()
+def filter_lessons(relevant_lessons):
+    filtered_lessons = filter_by_date(relevant_lessons)
+    if type_matter:
+        filtered_lessons = filter_lessons_by_type(filtered_lessons)
+    if location_matter:
+        filtered_lessons = filter_by_location(filtered_lessons)
+    return filtered_lessons
 
+
+def get_relevant_lessons():
+    sorted_entries = get_all_lessons()
+    lessons_with_name = find_all_lessons_with_name(sorted_entries)
+
+    relevant_lessons = filter_lessons(lessons_with_name)
+    return relevant_lessons
+
+
+def main():
+    try:
+        relevant_lessons = get_relevant_lessons()
+        if len(relevant_lessons) != 0:
+            print_lessons_enrolling_to(relevant_lessons)
+            enroll_all = confirm_enrollment_in_all()
+            if enroll_all:
+                for lesson in relevant_lessons:
+                    course_id = get_course_id(lesson)
+                    enrol_in_lesson(course_id)
+            else:
+                for lesson in relevant_lessons:
+                    print_lesson_information(lesson)
+                    course_id = get_course_id(lesson)
+                    enroll = confirm_enrollment_in_single_lesson(lesson)
+                    if enroll:
+                        enrol_in_lesson(course_id)
+        else:
+            print("no lessons found!")
+    except Exception as e:
+        print(e)
+    print("end")
+
+
+main()
